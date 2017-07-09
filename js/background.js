e9dfc6da57ed0b8beb0bc52a2ca8878f97d147bd
Adam <xadamxk@gmail.com>
2017-07-09 13:19:39 -0500

    Options Working
    Test Options Sync Functional

--- js/background.js
+++ js/background.js
@@ -1,6 +1,3 @@
-// Copyright (c) 2012 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
 console.log("HELLO WORLD");
 // This event is fired each time the user updates the text in the omnibox,
 // as long as the extension's keyword mode is still active.
